a01e7f9957c46ce3e1a805b56deac2fe3c0619e8
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-25 02:10:15 +0530

    testing

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
             });
         } else { 
             if(!logDate) {
-                console.log(Date.now().getFullYear());
+                console.log(new Date().getFullYear().getDate().getMonth());
             }
         }
     }
