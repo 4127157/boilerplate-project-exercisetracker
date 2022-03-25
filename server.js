6d3dbc44df8df9160f3e146e440e931f34cbe092
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-25 17:48:31 +0530

    testing

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/api/users/:id/logs', async (req, res) => {
         }
         return temp;
     }
-    console.log( await findUser().username);
+    console.log(await findUser());
 
 });
 
